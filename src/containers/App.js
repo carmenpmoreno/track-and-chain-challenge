d45a1f09c4e7568058f91002a1fd895839d8441b
carmenpmoreno <carmenp.moreno@gmail.com>
2019-07-31 21:56:55 +0200

    intial animation on chart lines

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
 import icoMenu from '../images/ico-menu.svg';
+import barChartImage from '../images/bar-chart.png';
 
 function App() {
   return (
@@ -9,32 +10,39 @@ function App() {
         <h1 className="header-section__title">Dashboard</h1>
         <nav className="user-menu">
           <div>
-            <i className="fas fa-user-circle" alt="Default avatar"/>
+            <i className="fas fa-user-circle" alt="Default avatar" />
           </div>
           <button className="user-menu__button">
-            <i className="fas fa-arrow-down"  alt="" />
+            <i className="fas fa-arrow-down" alt="" />
           </button>
         </nav>
       </header>
       <main className="main-section">
-        <nav className="stadistics-menu">
-          <button className="stadistics-menu__button">
+        <nav className="charts-menu">
+          <button className="charts-menu__button">
             <img src={icoMenu} alt=""></img>
           </button>
-          <ul className="stadistics-menu__list">
-            <li className="stadistics-menu__item">
+          <ul className="charts-menu__list">
+            <li className="charts-menu__item">
               <i className="fas fa-chart-bar" alt="" />
             </li>
-            <li className="stadistics-menu__item">
+            <li className="charts-menu__item">
               <i className="fas fa-chart-line" alt="" />
             </li>
-            <li className="stadistics-menu__item">
+            <li className="charts-menu__item">
               <i className="fas fa-table" alt="" />
             </li>
-
           </ul>
         </nav>
-        <section className="">
+        <section className="charts-section">
+          <article className="charts-article">
+            <div className="charts-article__line"></div>
+            <div className="charts-article__circle"></div>
+            <div className="charts-article__line"></div>
+            <div className="charts-article__image-wrapper">
+              <img className="charts-article__default-image" src={barChartImage} alt="default Bar Chart" />
+            </div>
+          </article>
         </section>
       </main>
 
