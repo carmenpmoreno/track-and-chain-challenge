2deb2238a2ace984a48d8c64ff73015b433c71d0
carmenpmoreno <carmenp.moreno@gmail.com>
2019-08-02 18:57:30 +0200

    build docs

---
+++ docs/precache-manifest.092708de8b7c34de3c9c5ae6a81440e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f887f2ab152bb405276cb684eeb38c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcbc33a9ca3dc07e9e5c",
+    "url": "./static/css/main.277e316a.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "./static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "dcbc33a9ca3dc07e9e5c",
+    "url": "./static/js/main.117d3871.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1bbcc61cd8288f158e924d8a146e539b",
+    "url": "./static/media/bar-chart.1bbcc61c.png"
+  },
+  {
+    "revision": "f3a1eda4fb2619b540b4828bc0f4846c",
+    "url": "./static/media/ico-menu.f3a1eda4.svg"
+  },
+  {
+    "revision": "40bb8f7ef9dc2144f90c4b62639dab1d",
+    "url": "./static/media/temperature-changes-chart.40bb8f7e.png"
+  }
+]);
