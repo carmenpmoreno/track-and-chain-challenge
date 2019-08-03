c12ab6379347b074f759022d2f09f5e952f0a746
carmenpmoreno <carmenp.moreno@gmail.com>
2019-08-03 13:53:14 +0200

    new docs with tablet sizes

---
+++ docs/precache-manifest.bbc6f693071364fab28c0b34a70010ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e5771b74354ff18baff5d28d704e06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6059490bfe94863060f4",
+    "url": "./static/css/main.8ec6aa8b.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "./static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "6059490bfe94863060f4",
+    "url": "./static/js/main.20490d11.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1bbcc61cd8288f158e924d8a146e539b",
+    "url": "./static/media/bar-chart.1bbcc61c.png"
+  },
+  {
+    "revision": "f3a1eda4fb2619b540b4828bc0f4846c",
+    "url": "./static/media/ico-menu.f3a1eda4.svg"
+  },
+  {
+    "revision": "40bb8f7ef9dc2144f90c4b62639dab1d",
+    "url": "./static/media/temperature-changes-chart.40bb8f7e.png"
+  }
+]);
