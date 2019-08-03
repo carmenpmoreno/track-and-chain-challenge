7df6201a54dc266bb7f331781789a45c78f20d2b
carmenpmoreno <carmenp.moreno@gmail.com>
2019-08-03 20:18:39 +0200

    build docs on dev

---
+++ docs/precache-manifest.edc3456b8adac2c25975d005db3fef7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b073554387e17da067020dcc528e685",
+    "url": "./index.html"
+  },
+  {
+    "revision": "492dd39f6a7f8b3d0698",
+    "url": "./static/css/main.83a84cde.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "./static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "492dd39f6a7f8b3d0698",
+    "url": "./static/js/main.ae22eb8b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1bbcc61cd8288f158e924d8a146e539b",
+    "url": "./static/media/bar-chart.1bbcc61c.png"
+  },
+  {
+    "revision": "40bb8f7ef9dc2144f90c4b62639dab1d",
+    "url": "./static/media/temperature-changes-chart.40bb8f7e.png"
+  }
+]);
