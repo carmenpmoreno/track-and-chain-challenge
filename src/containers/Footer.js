8cb0b7889f6fad18ac455d1ad44e3f8928cc4d57
carmenpmoreno <carmenp.moreno@gmail.com>
2019-08-07 20:10:23 +0200

    add paragraph on footer

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -5,8 +5,9 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="footer-section">
+        <p className="footer-section__p">Track&Chain challenge</p>
         <small className="footer-section__copy"> ©Copy Dashboard</small>
-        <p className="footer-section__p">Powered by Carmen</p>
+        <p className="footer-section__p">Powered by Carmen Pilar</p>
       </footer>
     );
   }
