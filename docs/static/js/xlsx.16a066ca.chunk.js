a37541c209dafbc7ac895759da2adc813c6fae40
carmenpmoreno <carmenp.moreno@gmail.com>
2019-08-07 14:41:32 +0200

    build new docs

--- docs/static/js/xlsx.16a066ca.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{160:function(n,o){},171:function(n,o){},172:function(n,o){}}]);
-//# sourceMappingURL=xlsx.16a066ca.chunk.js.map
---
+++ docs/static/js/xlsx.87ea6553.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{161:function(n,o){},172:function(n,o){},173:function(n,o){}}]);
+//# sourceMappingURL=xlsx.87ea6553.chunk.js.map
